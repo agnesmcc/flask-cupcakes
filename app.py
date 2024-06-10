4057695b49d6dc759706c01180e1b5e09d2a0db2
Agnes <agnes.mccall5@gmail.com>
2024-06-09 21:44:40 -0400

    finish part two

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 """Flask app for Cupcakes"""
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from models import db, connect_db, Cupcake
 
 app = Flask(__name__)
@@ -11,3 +11,25 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 connect_db(app)
+
+@app.route('/api/cupcakes', methods=['GET', 'POST'])
+def get_cupcakes():
+    if request.method == 'GET':
+        return jsonify(cupcakes=[c.serialize() for c in Cupcake.query.all()])
+    else:
+        flavor = request.json['flavor']
+        size = request.json['size']
+        rating = request.json['rating']
+        image = request.json['image']
+        cupcake = Cupcake(flavor=flavor, size=size, rating=rating, image=image)
+        db.session.add(cupcake)
+        db.session.commit()
+        response = jsonify(cupcake=cupcake.serialize())
+        response.status_code = 201
+        return response
+
+
+@app.route('/api/cupcakes/<int:cupcake_id>', methods=['GET'])
+def get_cupcake(cupcake_id):
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    return jsonify(cupcake=cupcake.serialize())
--- models.py
+++ models.py
@@ -18,3 +18,12 @@ class Cupcake(db.Model):
     size = db.Column(db.Text, nullable=False)
     rating = db.Column(db.Float, nullable=False)
     image = db.Column(db.Text, default="https://tinyurl.com/demo-cupcake")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "flavor": self.flavor,
+            "size": self.size,
+            "rating": self.rating,
+            "image": self.image
+        }
